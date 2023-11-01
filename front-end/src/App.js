23b347148bd8ca1ea97df6f5ce8b3ac6bb0b053a
M.B.A. Fadeyi <fadeyibajide@gmail.com>
2023-11-01 15:09:58 +0000

    React Router Dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+function JobList() {
   const [jobs, setJobs] = useState(null);
 
   useEffect(() => {
@@ -17,10 +18,50 @@ function App() {
     <div>
       {jobs &&
         jobs.map((job, i) => {
-          <div key={i}> a new job</div>;
+          return (
+            <div key={i}>
+              {" "}
+              Job #{job.id}: {job.title}{" "}
+            </div>
+          );
         })}
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Routes> looks through its children <Route>s and renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/users" element={<Users />}></Route>
+          <Route path="/" element={<JobList />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
